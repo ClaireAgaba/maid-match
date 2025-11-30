89a59ce1d314e8a726637d07e28faa99366be616
ClaireAgaba <claireagaba77@gmail.com>
2025-12-01 02:14:16 +0300

    deploy

--- web/src/services/api.js
+++ web/src/services/api.js
@@ -1,7 +1,11 @@
 import axios from 'axios';
 
 //const API_BASE_URL = 'http://localhost:8000/api';
-const API_BASE_URL = import.meta.env.VITE_API_URL;
+//const API_BASE_URL = import.meta.env.VITE_API_URL;
+
+const API_BASE_URL = import.meta.env.DEV
+  ? 'http://localhost:8000/api'          // used in local dev
+  : import.meta.env.VITE_API_URL;        // used in Netlify/prod
 
 // Create axios instance with default config
 const api = axios.create({
