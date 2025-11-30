eec704f8ec126b3d94c0fe82ddf1de4314da02f3
ClaireAgaba <claireagaba77@gmail.com>
2025-11-30 21:46:33 +0300

    push all

--- web/src/services/api.js
+++ web/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'http://localhost:8000/api';
-//const API_BASE_URL = import.meta.env.VITE_API_URL;
+//const API_BASE_URL = 'http://localhost:8000/api';
+const API_BASE_URL = import.meta.env.VITE_API_URL;
 
 // Function to get CSRF token from cookie
 const getCsrfToken = () => {
