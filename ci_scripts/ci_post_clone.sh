c821eed96eaafd15aec2e3661edd3d61d826a64f
ANKUNDA GRACE STELLA <ankundagracestella@ANKUNDAs-MacBook-Air.local>
2025-12-17 15:50:06 +0300

    Install CocoaPods in Xcode Cloud if missing

--- ci_scripts/ci_post_clone.sh
+++ ci_scripts/ci_post_clone.sh
@@ -10,4 +10,13 @@ npm ci
 npx cap sync ios
 
 cd ios/App
+pwd
+ls -la
+
+if ! command -v pod >/dev/null 2>&1; then
+  gem install --user-install cocoapods -N
+  export PATH="$HOME/.gem/ruby/$(ruby -e 'require "rbconfig"; print RbConfig::CONFIG["ruby_version"]')/bin:$PATH"
+fi
+
+pod --version
 pod install
--- ci_scripts/ci_pre_xcodebuild.sh
+++ ci_scripts/ci_pre_xcodebuild.sh
@@ -5,4 +5,10 @@ cd mobile
 npx cap sync ios
 
 cd ios/App
+if ! command -v pod >/dev/null 2>&1; then
+  gem install --user-install cocoapods -N
+  export PATH="$HOME/.gem/ruby/$(ruby -e 'require "rbconfig"; print RbConfig::CONFIG["ruby_version"]')/bin:$PATH"
+fi
+
+pod --version
 pod install
